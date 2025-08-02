f1971515bbbbd5aa2c1c0b6c668250ec22b3ddd5
yash0patel <yashpatelrupal@gmail.com>
2025-08-02 22:40:56 +0530

    Add GridLayout with ImageButtons and EditText for bid value in GamePlayActivity for diamond game

--- android25/app/src/main/java/com/royal/android25/HomeActivity.java
+++ android25/app/src/main/java/com/royal/android25/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.royal.android25;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btnStartGame, btnScoreBoard, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,19 @@ public class HomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+//        bind
+        btnScoreBoard = findViewById(R.id.btnHomeScoreBoard);
+        btnLogout = findViewById(R.id.btnHomeLogout);
+        btnStartGame = findViewById(R.id.btnHomeStartGame);
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GamePlayActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
